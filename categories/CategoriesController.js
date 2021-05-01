e3d1ed843a689c44d7197ab5038fc8529d11fa3a
Karin Morais <andradkarin@gmail.com>
2021-05-01 20:30:31 -0300

    Inicio da listagem das categorias

--- categories/CategoriesController.js
+++ categories/CategoriesController.js
@@ -1,9 +1,31 @@
 const express = require("express");
 const router = express.Router();
+const Category = require("./Category");
+const slugify = require("slugify");
 
 router.get("/admin/categories/new", (req, res) => {
 
         res.render("admin/categories/new.ejs");
 });
+
+router.post("/categories/save", (req, res) => {
+    var categoria = req.body.title;
+    if(categoria.trim() != undefined && categoria.trim() != ""){
+
+        Category.create({
+            title: categoria,
+            slug: slugify(categoria.toLowerCase()) 
+        }).then(() => {
+            res.redirect("/")
+        });
+
+    }else{
+        res.redirect("/admin/caregories/new");
+    }
+});
+
+router.get("/admin/categories", (req, res) => {
+        res.render("admin/categories/index");
+})
 
 module.exports = router;

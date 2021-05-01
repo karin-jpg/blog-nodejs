c048f695da38dd5c2db7395313b324ccfb089da6
Karin Morais <andradkarin@gmail.com>
2021-05-01 14:49:06 -0300

    criação do arquivo de conexao com o banco

--- index.js
+++ index.js
@@ -1,8 +1,13 @@
+const bodyParser = require("body-parser");
 const express = require("express")
 const app = express();
 
 app.set('view engine', 'ejs');
 
+app.use(express.static('public'));
+
+app.use(express.urlencoded({extends: false}));
+app.use(express.json());
 
 app.get("/", (req, res) => {
 

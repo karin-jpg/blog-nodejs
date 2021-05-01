e32ba10bbd1169a3bf78c49911ef643d06e45435
Karin Morais <andradkarin@gmail.com>
2021-05-01 14:29:09 -0300

    Inicio da criação da pagina principal do blog

--- index.js
+++ index.js
@@ -1,15 +1,15 @@
 const express = require("express")
 const app = express();
 
-
+app.set('view engine', 'ejs');
 
 
 app.get("/", (req, res) => {
 
-    res.send("Dale")
+    res.render("index.ejs");
 
 })
 
 app.listen(3000, () => {
-    console.log("vai papai")
+    console.log("servidor ta on");
 })

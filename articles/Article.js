53ae27d36b6b4673f40602006bf59ba2b16046a4
Karin Morais <andradkarin@gmail.com>
2021-05-01 19:09:05 -0300

    Adicionada relaçao entre Category e Article

--- articles/Article.js
+++ articles/Article.js
@@ -1,5 +1,6 @@
 const Sequelize = require("sequelize");
 const connection = require("../database/database");
+const Category = require("./../categories/Category");
 
 
 const Article = connection.define("articles", {
@@ -16,5 +17,8 @@ const Article = connection.define("articles", {
         allowNull: false
     }
 });
+
+Article.belongsTo(Category);
+
 
 module.exports = Article;

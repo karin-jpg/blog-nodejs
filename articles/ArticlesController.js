0565b697cf1eff0dc973b5689896fb34aaa29b4a
karin-jpg <andradkarin@gmail.com>
2021-05-02 10:44:32 -0300

    Adicionada rota para criação de artigo

--- articles/ArticlesController.js
+++ articles/ArticlesController.js
@@ -1,8 +1,10 @@
 const express = require("express");
 const router = express.Router();
-const Category = require("../categories/Category")
+const Category = require("../categories/Category");
+const Article = require("./Article");
+const slugify = require("slugify");
 
-router.get("/articles", (req, res) => {
+router.get("/admin/articles", (req, res) => {
 
     res.send("Rota de artigos");
 
@@ -15,5 +17,22 @@ router.get("/admin/articles/new", (req, res) => {
         res.render("admin/articles/new", {categories: categories});
     })
 });
+
+router.post("/articles/save", (req, res) => {
+    var title = req.body.title;
+    var body = req.body.body;
+    var category = req.body.category;
+    console.log(title);
+    console.log(body);
+    console.log(category);
+    Article.create({
+        title:title,
+        body: body,
+        categoryId: category,
+        slug:  slugify(title.toLowerCase())
+    }).then(() => {
+        res.redirect("/admin/articles");
+    })
+})
 
 module.exports = router;

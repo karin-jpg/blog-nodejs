1c799a32037c271b7f5cdc376915ea146546fc38
karin-jpg <andradkarin@gmail.com>
2021-05-02 13:19:45 -0300

    Adicionada link de referencia para leitura de artigo e pagina de leitura de artigo

--- index.js
+++ index.js
@@ -32,11 +32,35 @@ app.use("/", articlesController);
 app.get("/", (req, res) => {
 
     Article
-    .findAll()
+    .findAll({
+        order: [
+            ['id', 'DESC']
+        ]
+    })
     .then((articles) => {
         res.render("index", {articles: articles});
     });
     
+});
+
+app.get("/:slug", (req, res) => {
+    var slug = req.params.slug;
+
+    Article.findOne({
+        where: {
+            slug: slug
+        }
+    })
+    .then((article) => {
+        if(article != undefined){
+            res.render("article", {article: article});
+        }else{
+            res.redirect("/");
+        }
+    })
+    .catch((error) => {
+        res.redirect("/");
+    });
 });
 
 app.listen(8080, () => {

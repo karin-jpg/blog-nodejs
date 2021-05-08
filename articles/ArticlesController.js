fcbdfd327cbfbd543c7def556cc688aae9bea277
Karin Morais <andradkarin@gmail.com>
2021-05-08 19:48:35 -0300

    Adição da edição de artigos

--- articles/ArticlesController.js
+++ articles/ArticlesController.js
@@ -24,6 +24,31 @@ router.get("/admin/articles/new", (req, res) => {
     })
 });
 
+router.get("/admin/articles/edit/:id", (req, res) => {
+    var id = req.params.id;
+
+    if(isNaN(id)){
+        res.redirect("/admin/articles");
+    }
+
+    Article
+    .findOne()
+    .then((article) => {
+        if(article != undefined){
+            Category.findAll().then((categories) => {
+                res.render("admin/articles/edit", {article: article, categories: categories})
+            });
+            
+        }else{
+            res.redirect("/admin/articles");
+        }
+    })
+    .catch(erro => {
+        res.redirect("/admin/articles");
+    });
+});
+
+
 router.post("/articles/save", (req, res) => {
     var title = req.body.title;
     var body = req.body.body;
@@ -39,6 +64,23 @@ router.post("/articles/save", (req, res) => {
     }).then(() => {
         res.redirect("/admin/articles");
     })
+});
+
+router.post("/articles/update", (req, res) => {
+    var id = req.body.id;
+    var title = req.body.title;
+    var body = req.body.body;
+    var category = req.body.category;
+
+    Article.update({title: title, body: body, slug: slugify(title.toLowerCase()), categoryId: category},
+    {
+        where: {
+            id : id
+        }
+    })
+    .then(() => {
+        res.redirect("/admin/articles");
+    });
 });
 
 router.post("/articles/delete", (req, res) => {
